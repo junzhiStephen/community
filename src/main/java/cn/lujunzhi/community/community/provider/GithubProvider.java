d58c124a5d4b77c588ca52833a88e1a6e48dcaf8
lujunzhi <969475706@qq.com>
2020-03-18 22:00:38 +0800

    extract github config

---
+++ src/main/java/cn/lujunzhi/community/community/provider/GithubProvider.java
@@ -0,0 +1,54 @@
+package cn.lujunzhi.community.community.provider;
+
+import cn.lujunzhi.community.community.dto.AccessTokenDTO;
+import cn.lujunzhi.community.community.dto.GithubUser;
+import com.alibaba.fastjson.JSON;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+@Component
+public class GithubProvider {
+    public  String getAccessToken(AccessTokenDTO accessTokenDTO){
+        MediaType mediaType
+                =MediaType.get("application/json;charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body=RequestBody.create(mediaType,JSON.toJSONString(accessTokenDTO));
+            Request request = new Request.Builder()
+                    .url("https://github.com/login/oauth/access_token")
+                    .post(body)
+                    .build();
+            try(Response response =client.newCall(request).execute()){
+                String string =response.body().string();
+               String token =string.split("&")[0].split("=")[1];
+               System.out.println(string);
+                return  token;
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+        public GithubUser  getUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user")
+                .header("Authorization","token "+accessToken)
+                .build();
+            try {
+                Response response = client.newCall(request).execute();
+                assert response.body()!=null;
+                String string = response.body().string();
+                System.out.println(string);
+                GithubUser githubUser= com.alibaba.fastjson.JSON.parseObject(string,GithubUser.class);
+
+                return githubUser;
+            }catch (IOException e) {
+
+                e.printStackTrace();
+
+            }
+
+        return null;
+    }
+}
+
